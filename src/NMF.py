d5ea911743716d8d19e2faa836f8b0bb6a2a4323
sehinger <sylvain.ehinger62@gmail.com>
2018-05-29 10:58:04 +0200

    Commit #4 : Premier essai de deep nmf, test sur image

--- src/NMF.py
+++ src/NMF.py
@@ -5,6 +5,10 @@ import matplotlib.image as mp
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 import random
+import theano
+import theano.tensor as T
+
+from scipy.sparse.linalg import svds
 
 
 def rgb2gray(rgb):
@@ -14,28 +18,52 @@ def rgb2gray(rgb):
     
 gray = mp.imread("../black.png")
 gray = rgb2gray(gray)
-msk = np.zeros(gray.size)
+	       
 
-for i in range(0,len(msk)):
-	msk[i] = random.choice([0, 1])
-	
+def appr_seminmf(M, r):
+    """
+        Approximate Semi-NMF factorisation. 
+        
+        Parameters
+        ----------
+        M: array-like, shape=(n_features, n_samples)
+        r: number of components to keep during factorisation
+    """
+    
+    if r < 2:
+        raise ValueError("The number of components (r) has to be >=2.")
+
+    A, S, B = svds(M, r-1)
+    S = np.diag(S)
+    A = np.dot(A, S)
+ 
+    m, n = M.shape
+ 
+    for i in range(r-1):
+        if B[i, :].min() < (-B[i, :]).min():
+            B[i, :] = -B[i, :]
+            A[:, i] = -A[:, i]
+            
+            
+    if r == 2:
+        U = np.concatenate([A, -A], axis=1)
+    else:
+        An = -np.sum(A, 1).reshape(A.shape[0], 1)
+        U = np.concatenate([A, An], 1)
+    
+    V = np.concatenate([B, np.zeros((1, n))], 0)
+
+    if r>=3:
+        V -= np.minimum(0, B.min(0))
+    else:
+        V -= np.minimum(0, B)
+
+    return U, V
+
+n,m =gray.shape
 
 
-gray = ma.array(gray, mask = msk)
-
-plt.imshow(gray,cmap = plt.get_cmap('gray'))
-plt.show()
-           
-print(gray)
-              
-model = NMF(n_components=1, init='random', random_state=0, beta_loss='frobenius', max_iter=500, tol=0.0001)
-W = model.fit_transform(gray)
-H = model.components_
-
-
-X1 = W.dot(H)
-print(X1)
-
-plt.imshow(X1,cmap = plt.get_cmap('gray'))
-plt.show()
-
+for i in range(2):
+	appr_seminmf(gray,m)
+	m=m/2
+	print(gray)
